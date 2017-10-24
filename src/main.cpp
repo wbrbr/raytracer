3040c9672ce36c62eeb7acb612e9442c5bd397ad
Wilhem Barbier <wilhem.b@free.fr>
2017-10-24 16:16:15 +0200

    Basic specular lighting !

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main()
     sphere.radius = 0.3f;
 
     Light light;
-    light.position = Eigen::Vector3f(0.5f, 0.f, -1.f);
+    light.position = Eigen::Vector3f(0.5f, 0.f, -0.5f);
 
     const auto RED = Color{1.f, 0.f, 0.f, 1.f};
     const auto WHITE = Color{1.f, 1.f, 1.f, 1.f};
@@ -47,13 +47,25 @@ int main()
             Eigen::Vector3f closest_point = camera_ray.origin + closest * camera_ray.orientation;
             Eigen::Vector3f normal = (closest_point - sphere.center).normalized();
 
-            Eigen::Vector3f reflected = 2.f * (normal.dot(light_ray.orientation)) * normal - light_ray.orientation;
+            Eigen::Vector3f reflected = light_ray.orientation - 2.f * (normal.dot(light_ray.orientation)) * normal;
+            Eigen::Vector3f v = -camera_ray.orientation;
+            float v_dot_r = v.dot(reflected);
+            float specular_intensity;
+            if (v_dot_r < 0.f) {
+                specular_intensity = 0.f;
+            } else {
+                specular_intensity = pow(v_dot_r, 50);
+            }
 
             float diffuse_intensity = normal.dot(-light_ray.orientation);
             if (diffuse_intensity < 0.f) {
                 diffuse_intensity = 0.f;
             }
-            img.setPixel(x, y, Color{diffuse_intensity, 0.f, 0.f, 1.f});
+
+            float kd = 0.8f;
+            float ks = 0.2f;
+
+            img.setPixel(x, y, Color{diffuse_intensity * kd + specular_intensity * ks, specular_intensity * ks, specular_intensity * ks, 1.f});
         }
     }
     img.write("out.png");
